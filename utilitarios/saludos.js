fc21c41322ebfcf75e60dfb2980678d55a898af8
Joao <joaopriss@gmail.com>
2024-01-23 16:10:34 -0500

    se separa las funciones en el archivo utilitario.js

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -13,39 +13,3 @@ saludar=function(){
 
     mostraTextoEncaja("txtNombre"," ")
 }
-mostrarTexto=function(idComponente, mensaje){
-    let componente;
-    componente = document.getElementById(idComponente);
-    componente.innerText = mensaje;
-}
-
-mostraTextoEncaja=function(idComponente,mensaje){
-    let componente;
-    componente=document.getElementById(idComponente);
-    componente.value=mensaje;
-
-}
-
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente = document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-}
-mostrarImagen=function(idComponente,rutaImagen){
-    let imagen;
-    imagen=document.getElementById(idComponente);
-    imagen.src=rutaImagen;
-}
-
-recuperarInt = function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    let valorEntero = parseInt(valorCaja) ;
-    return valorEntero;
-}
-recuperarFloat = function(idComponente){
-    let valorEntero = recuperarTexto(idComponente);
-    let valorDecimal = parseFloat(valorEntero);
-    return valorDecimal;
-}
