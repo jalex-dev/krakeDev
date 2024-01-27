6d8d79ea7d7beb113cae0f4df5f9fcbad96ac354
Joao <joaopriss@gmail.com>
2024-01-26 21:08:27 -0500

    funcion obtener provincia y obtener tipo vehiculos implementado

--- placas/placas.js
+++ placas/placas.js
@@ -1,6 +1,8 @@
 validarPlaca = function () {
     let placa;
     let erroresEstructura;
+    let provincia;
+    let tipoVehiculo;
     placa = recuperarTexto("txtPlaca");
     mostrarTexto("lblMensaje", " ");
 
@@ -8,7 +10,11 @@ validarPlaca = function () {
     if (erroresEstructura == "") {
         mostrarTexto("lblMensaje", erroresEstructura);
 
-        mostrarTexto("lblValida", "ESTRUCTURA VALIDA")
+        mostrarTexto("lblValida", "ESTRUCTURA VALIDA");
+        provincia =obternerProvincia(placa);
+        mostrarTexto("lblProvincia",provincia);
+        tipoVehiculo=obtenerTipoVehiculo(placa);
+        mostrarTexto("lblTipoVehiculo",tipoVehiculo)
     } else {
         mostrarTexto("lblMensaje", erroresEstructura);
         mostrarTexto("lblValida", "ESTRUCTURA INCORRECTA")
--- placas/servicioPlacas.js
+++ placas/servicioPlacas.js
@@ -97,4 +97,91 @@ mensajeErrorDigitos=function(posicion) {
         return "El octavo carácter debe ser un dígito";
     }
 }
+obternerProvincia=function(placa){
 
+    // Obtener la primera letra de la placa (código de provincia)
+    let primeraLetra = placa.charAt(0);
+
+    // Asignar provincias basadas en la primera letra
+    if (primeraLetra === "A") {
+        return "Azuay";
+    } else if (primeraLetra === "B") {
+        return "Bolívar";
+    } else if (primeraLetra === "U") {
+        return "Cañar";
+    } else if (primeraLetra === "C") {
+        return "Carchi";
+    } else if (primeraLetra === "X") {
+        return "Cotopaxi";
+    } else if (primeraLetra === "H") {
+        return "Chimborazo";
+    } else if (primeraLetra === "O") {
+        return "El Oro";
+    } else if (primeraLetra === "E") {
+        return "Esmeraldas";
+    } else if (primeraLetra === "W") {
+        return "Galápagos";
+    } else if (primeraLetra === "G") {
+        return "Guayas";
+    } else if (primeraLetra === "I") {
+        return "Imbabura";
+    } else if (primeraLetra === "L") {
+        return "Loja";
+    } else if (primeraLetra === "R") {
+        return "Los Ríos";
+    } else if (primeraLetra === "M") {
+        return "Manabí";
+    } else if (primeraLetra === "V") {
+        return "Morona Santiago";
+    } else if (primeraLetra === "N") {
+        return "Napo";
+    } else if (primeraLetra === "S") {
+        return "Pastaza";
+    } else if (primeraLetra === "P") {
+        return "Pichincha";
+    } else if (primeraLetra === "K") {
+        return "Sucumbíos";
+    } else if (primeraLetra === "Q") {
+        return "Orellana";
+    } else if (primeraLetra === "T") {
+        return "Tungurahua";
+    } else if (primeraLetra === "Z") {
+        return "Zamora-Chinchipe";
+    } else if (primeraLetra === "Y") {
+        return "Santa Elena";
+    }
+
+    // Si no coincide con ninguna condición, retornar un mensaje indicando que la provincia no fue encontrada
+    return "Provincia incorrecta";
+
+}
+
+obtenerTipoVehiculo=function(placa){
+     // Obtener la segunda letra de la placa (código de tipo de vehículo)
+     let segundaLetra = placa.charAt(1);
+
+     // Asignar tipo de vehículo basado en la segunda letra
+     if (segundaLetra == "A" || segundaLetra == "Z") {
+         return "Vehículo Comercial";
+     } else if (segundaLetra == "E") {
+         return "Vehículo Gubernamental";
+     } else if (segundaLetra == "X") {
+         return "Vehículo de Uso Oficial";
+     } else if (segundaLetra == "S") {
+         return "Vehículo de Gobiernos Provinciales";
+     } else if (segundaLetra == "M") {
+         return "Vehículo Municipal";
+     } else if (
+         segundaLetra != "A" &&
+         segundaLetra != "E" &&
+         segundaLetra != "X" &&
+         segundaLetra != "S" &&
+         segundaLetra != "M"
+     ) {
+         return "Vehículo Particular";
+     }
+ 
+     // Si no coincide con ninguna condición, retornar un mensaje indicando que el tipo de vehículo no fue encontrado
+     return "vehículo Incorrecto";
+
+}
