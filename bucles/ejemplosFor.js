6d409b75cb88170750c26f4bcb8a00949e8aa011
Joao <joaopriss@gmail.com>
2024-01-27 19:06:36 -0500

    implementando funcion mostrar impares

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,44 @@
+mostrarNumeros=function(){
+    console.log("antes del for");
+    for(let i=0;i<4;i++){
+        console.log(i)
+    }
+    console.log("despues del for");
+}
+
+mostrarNumeros2=function(){
+    console.log("antes del for");
+    for(let i=1;i<=5;i++){
+        console.log(i)
+    }
+    console.log("despues del for");
+}
+
+mostrarPares=function(){
+    console.log("antes del for");
+    for(let x=0;x<=10;x+=2){
+        console.log(x)
+    }
+    console.log("despues del for");
+}
+
+mostrarInverso=function(){
+    console.log("antes del for");
+    for(let x=10;x>=0;x--){
+        console.log(x)
+    }
+    console.log("despues del for");
+}
+hackearNasaPelis=function(){
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("Hackeando nasa "+porcentaje+" %")
+    }
+    console.log("La nasa ha sido hackeada... :)");
+}
+mostrarImpares=function(){
+    console.log("antes del for");
+    for(let x=1;x<=21;x+=2){
+        console.log(x)
+    }
+    console.log("despues del for");
+}
