2c0b0308cc024ef29f42f57b733bd4fc057270b9
Joao <joaopriss@gmail.com>
2024-01-28 23:05:08 -0500

    paso 2 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,9 +1,38 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
 esMayuscula=function(caracter) {
     let code = caracter.charCodeAt();
 
     // Rango de códigos ASCII correspondiente a las letras mayúsculas A-Z
     if (code >= 65 && code <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// Función para ingresar palabras secretas 
+guardarPalabra=function(){
+    let palabra;
+    let caracter;
+    tieneMiniscula = false;
+    palabra = recuperarTexto("txtSecreta")
+    if (!validarPalabra(palabra)) {
+        alert("DEBE INGRESAR UNA PALABRA DE CINCO LETRAS MAYUSUCLAS")
+    }else{
+        for (let i = 0; i < palabra.length; i++) {
+            caracter = palabra.charAt(i);
+            if (!esMayuscula(caracter)) {
+                alert("DEBE INGRESAR UNA PALABRA DE CINCO LETRAS MAYUSUCLAS")
+            }    
+        }  
+        palabraSecreta=palabra;     
+    }
+    console.log(palabraSecreta)
+
+}
+
+validarPalabra=function(palabra) {
+    if (palabra.length  <= 5) {
         return true;
     } else {
         return false;
